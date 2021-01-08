d0a7198403b1b823dc9bad7f135fe84ba4367526
Thanwar <thawardas25@gmail.com>
2021-01-09 01:00:51 +0500

    final version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
 
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Taskbox</h1>
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
-
 export default App;
